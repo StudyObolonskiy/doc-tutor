923cba278630e47c187193987857e7229a0bcb2d
StudyObolonskiy <study.obolonskiy@gmail.com>
2024-02-29 12:45:43 +0300

    feat: finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,20 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://doc-tutor.vercel.sh'),
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
